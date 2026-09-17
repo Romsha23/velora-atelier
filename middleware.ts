c9bd94dfd1513b2c1bbd71fc7108e402b162214b
Romsha Wadhwa <romsha2303@gmail.com>
2026-09-17 10:48:37 +0530

    Fix 500 MIDDLEWARE_INVOCATION_FAILED by passing publishableKey & secretKey natively to clerkMiddleware options

--- middleware.ts
+++ middleware.ts
@@ -15,33 +15,21 @@ const isPublicRoute = createRouteMatcher([
   '/api(.*)',
 ]);
 
-const clerkHandler = clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    await auth.protect();
+export default clerkMiddleware(
+  async (auth, req) => {
+    if (!isPublicRoute(req)) {
+      await auth.protect();
+    }
+  },
+  {
+    publishableKey:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ||
+      'pk_test_ZWFzeS10YXBpci03MDM5LmNsZXJrLmFjY291bnRzLmRldiQ',
+    secretKey:
+      process.env.CLERK_SECRET_KEY ||
+      'sk_test_85SQ6L1URBzYQhVGGmOvQ4qaQEeaexrui1M1jyfUuA',
   }
-});
-
-export default function middleware(req: any, evt: any) {
-  const secretKey =
-    process.env.CLERK_SECRET_KEY ||
-    'sk_test_85SQ6L1URBzYQhVGGmOvQ4qaQEeaexrui1M1jyfUuA';
-  const publishableKey =
-    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ||
-    'pk_test_ZWFzeS10YXBpci03MDM5LmNsZXJrLmFjY291bnRzLmRldiQ';
-
-  if (!process.env.CLERK_SECRET_KEY) {
-    process.env.CLERK_SECRET_KEY = secretKey;
-  }
-  if (!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) {
-    process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY = publishableKey;
-  }
-
-  try {
-    return clerkHandler(req, evt);
-  } catch (err) {
-    return;
-  }
-}
+);
 
 export const config = {
   matcher: [
