d29f329aa5f731afe29a63de1e885cd0b6f197db
Romsha Wadhwa <romsha2303@gmail.com>
2026-09-17 11:40:32 +0530

    Restore standard Clerk middleware with Vercel Environment Variables

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,3 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
@@ -17,29 +15,11 @@ const isPublicRoute = createRouteMatcher([
   '/api(.*)',
 ]);
 
-const publishableKey =
-  process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY ||
-  'pk_test_ZWFzeS10YXBpci03MDM5LmNsZXJrLmFjY291bnRzLmRldiQ';
-
-const secretKey =
-  process.env.CLERK_SECRET_KEY ||
-  'sk_test_85SQ6L1URBzYQhVGGmOvQ4qaQEeaexrui1M1jyfUuA';
-
-export default function middleware(req: NextRequest, evt: any) {
-  try {
-    const handler = clerkMiddleware(
-      async (auth, request) => {
-        if (!isPublicRoute(request)) {
-          await auth.protect();
-        }
-      },
-      { publishableKey, secretKey }
-    );
-    return handler(req, evt);
-  } catch (error) {
-    return NextResponse.next();
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
   }
-}
+});
 
 export const config = {
   matcher: [
